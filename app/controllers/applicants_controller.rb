32910c036dbc89dacb5a79083ba4597b74ee66e6
Lorenzo Rafael T. Bautista <lorenzo.bautista@obf.ateneo.edu>
2016-05-07 15:51:23 +0800

    show_applicant

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -73,11 +73,7 @@ def room_select
   redirect_to applicants_path
 end
 
-def bed_select
-  @applicant = Applicant.find(params[:id])
-  @applicant.update_attribute(:room_number,params[:room_number])
-  redirect_to applicants_path
-end
+
   # PATCH/PUT /applicants/1
   # PATCH/PUT /applicants/1.json
   def update
