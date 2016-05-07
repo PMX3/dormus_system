8849ad26d06a3b2857697400009dfe7b90ffc93d
Lorenzo Rafael T. Bautista <lorenzo.bautista@obf.ateneo.edu>
2016-05-08 02:40:37 +0800

    with calendar for food_plan

--- app/models/food_plan.rb
+++ app/models/food_plan.rb
@@ -1,2 +1,5 @@
 class FoodPlan < ActiveRecord::Base
+	 def start_time
+        self.food_plan_date ##Where 'start' is a attribute of type 'Date' accessible through MyModel's relationship
+    end
 end
