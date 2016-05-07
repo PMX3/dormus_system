c8276a26d433ea11a3d8426e53a73268d5f78b90
Lorenzo Rafael T. Bautista <lorenzo.bautista@obf.ateneo.edu>
2016-05-07 15:33:41 +0800

    show_applicant

--- db/schema.rb
+++ db/schema.rb
@@ -60,6 +60,24 @@ ActiveRecord::Schema.define(version: 20160507055703) do
     t.datetime "updated_at",     null: false
   end
 
+  create_table "dormers", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "dormers", ["email"], name: "index_dormers_on_email", unique: true
+  add_index "dormers", ["reset_password_token"], name: "index_dormers_on_reset_password_token", unique: true
+
   create_table "food_plans", force: :cascade do |t|
     t.date     "food_plan_date"
     t.datetime "created_at",     null: false
