71a339545377b3a24d23d3a915fb465950db4682
bryanzerrudo <bryan.zerrudo@gmail.com>
2016-05-07 15:55:25 +0800

    Navbar Fixes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160507055703) do
+ActiveRecord::Schema.define(version: 20160507071930) do
 
   create_table "applicants", force: :cascade do |t|
     t.string   "last_name"
@@ -70,7 +70,7 @@ ActiveRecord::Schema.define(version: 20160507055703) do
     t.string   "name"
     t.string   "contact_number"
     t.text     "address"
-    t.string   "email_address"
+    t.string   "email"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
   end
