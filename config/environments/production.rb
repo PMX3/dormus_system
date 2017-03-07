ec00ac7d721ef16e0d0deeef02fc4d8335dcc780
Lorenzo Rafael T. Bautista <lorenzo.bautista@obf.ateneo.edu>
2017-03-07 09:26:48 +0800

    emailer yo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,6 +35,14 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'example.com',
+  user_name:            'dormusph@gmail.com',
+  password:             'chan3bautista4',
+  authentication:       'plain',
+  enable_starttls_auto: true  }
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
