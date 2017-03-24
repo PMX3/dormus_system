c85d7073eedb474b3ffc844c9fca13be76fb9b4c
Lorenzo Rafael T. Bautista <lorenzo.bautista@obf.ateneo.edu>
2017-03-24 22:13:07 +0800

    room functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,9 @@
 //= require jquery
 
 //= require jquery_ujs
-
+//= require jquery-ui  
 //= require autocomplete-rails
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+
