0d564e45746c178bd4565210f7a329694ce5e8a3
Lorenzo Rafael T. Bautista <lorenzo.bautista@obf.ateneo.edu>
2017-03-28 18:40:38 +0800

    boop

--- app/models/door_log.rb
+++ app/models/door_log.rb
@@ -1,4 +1,5 @@
 class DoorLog < ActiveRecord::Base
+	before_create :limitrecords
 	def self.to_csv
 		attributes=%w{time_enter direction dormer_id}
 		CSV.generate(headers:true) do |csv|
@@ -7,5 +8,9 @@ class DoorLog < ActiveRecord::Base
 				csv <<door_log.attributes.values_at(*attributes)
 			end
 		end
+	end
+
+	def limitrecords
+		DoorLog.first.destroy if DoorLog.count > 2300
 	end
 end
