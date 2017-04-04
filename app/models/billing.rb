4161f9a6262a6266f1b59b6555c28b6234dac9a6
Lorenzo Rafael T. Bautista <lorenzo.bautista@obf.ateneo.edu>
2017-04-04 23:17:07 +0800

    precompiled assets

--- app/models/billing.rb
+++ app/models/billing.rb
@@ -1,11 +1,23 @@
 class Billing < ActiveRecord::Base
 	def self.to_csv
-		attributes=%w{tenant_id bill_type description total_amount created_at datetime_paid paid }
+		attributes=%w{tenant_id bill_type_dec total_amount created_at datetime_paid paid }
 		CSV.generate(headers:true) do |csv|
 			csv<<attributes
 			all.each do |bill|
-				csv <<bill.attributes.values_at(*attributes)
+				csv <<bill.attributes.map{|attr| user.send(attr)}
 			end
+		end
+	end
+
+	def bill_type_dec
+		if bill_type!=nil
+			if bill_type==1
+				"Water"
+			else
+				"Electricity"
+			end
+		else
+			"#{description}"
 		end
 	end
 end
