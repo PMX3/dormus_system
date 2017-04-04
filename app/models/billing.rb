15f4cf975eb3702ee364deffd9a958a428896148
Lorenzo Rafael T. Bautista <lorenzo.bautista@obf.ateneo.edu>
2017-04-04 23:34:12 +0800

    billing to csv

--- app/models/billing.rb
+++ app/models/billing.rb
@@ -24,7 +24,7 @@ class Billing < ActiveRecord::Base
 	def tenant
 		@applicants=Applicants.all 
 		@applicants.each do |appli|
-			if appli.id==tenant_id
+			if appli.id==this.tenant_id
 				"#{appli.first_name} #{appli.last_name}"
 
 			end
